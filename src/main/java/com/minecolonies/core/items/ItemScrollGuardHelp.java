f31972c64dfeca42298df0c75d6a13efabcf9ed7
YoruNina <836193631@qq.com>
2024-07-19 16:33:50 +0800

    extract public method of summon guard follow player

--- src/main/java/com/minecolonies/core/items/ItemScrollGuardHelp.java
+++ src/main/java/com/minecolonies/core/items/ItemScrollGuardHelp.java
@@ -5,7 +5,6 @@ import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.colony.buildings.IBuilding;
 import com.minecolonies.api.entity.ai.statemachine.AIOneTimeEventTarget;
 import com.minecolonies.api.entity.ai.statemachine.states.AIWorkerState;
-import com.minecolonies.core.tileentities.TileEntityColonyBuilding;
 import com.minecolonies.api.util.BlockPosUtil;
 import com.minecolonies.api.util.MessageUtils;
 import com.minecolonies.api.util.SoundUtils;
@@ -16,21 +15,23 @@ import com.minecolonies.core.colony.buildings.modules.settings.GuardTaskSetting;
 import com.minecolonies.core.colony.jobs.AbstractJobGuard;
 import com.minecolonies.core.entity.ai.workers.guard.AbstractEntityAIGuard;
 import com.minecolonies.core.network.messages.client.VanillaParticleMessage;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.horse.Llama;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.sounds.SoundEvents;
+import com.minecolonies.core.tileentities.TileEntityColonyBuilding;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
-
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
@@ -39,11 +40,6 @@ import java.util.List;
 
 import static com.minecolonies.api.util.constant.Constants.TICKS_SECOND;
 import static com.minecolonies.api.util.constant.translation.ToolTranslationConstants.TOOL_GUARD_SCROLL_NO_GUARD_BUILDING;
-
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.Style;
 
 /**
  * Magic scroll which summons guards to the users aid, with a limited duration. Only works within the same world as the colony.
@@ -72,83 +68,61 @@ public class ItemScrollGuardHelp extends AbstractItemScroll
             MessageUtils.format(TOOL_GUARD_SCROLL_NO_GUARD_BUILDING).sendTo(player);
             return itemStack;
         }
-
+        summonGuardFollowPlayer(world, player, building, colony, TICKS_SECOND * 900);
+        SoundUtils.playSoundForPlayer(player, SoundEvents.UI_TOAST_CHALLENGE_COMPLETE, 0.3f, 1.0f);
         itemStack.shrink(1);
+
+        return itemStack;
+    }
+
+    public void summonGuardFollowPlayer(final Level world, final ServerPlayer player, final IBuilding building, final IColony colony, Integer lastTime) {
         final List<ICitizenData> guards = new ArrayList<>(building.getAllAssignedCitizen());
 
-        if (world.random.nextInt(10) == 0 || colony.getWorld() != world)
-        {
-            // Fail
-            final Llama entity = EntityType.LLAMA.create(world);
-            entity.setPos(player.getX(), player.getY(), player.getZ());
-            world.addFreshEntity(entity);
+        for (final ICitizenData citizenData : guards) {
+            final AbstractJobGuard job = citizenData.getJob(AbstractJobGuard.class);
+            if (job != null && job.getWorkerAI() != null && !((AbstractEntityAIGuard) job.getWorkerAI()).hasTool()) {
+                continue;
+            }
 
-            player.displayClientMessage(Component.translatable("minecolonies.scroll.failed" + (world.random.nextInt(FAIL_RESPONSES_TOTAL) + 1)).setStyle(Style.EMPTY.withColor(
-              ChatFormatting.GOLD)), true);
-
-            SoundUtils.playSoundForPlayer(player, SoundEvents.EVOKER_CAST_SPELL, 0.5f, 1.0f);
-            return itemStack;
-        }
-        else
-        {
-            for (final ICitizenData citizenData : guards)
-            {
-                final AbstractJobGuard job = citizenData.getJob(AbstractJobGuard.class);
-                if (job != null && job.getWorkerAI() != null && !((AbstractEntityAIGuard) job.getWorkerAI()).hasTool())
-                {
+            if (citizenData.getEntity().isPresent()) {
+                if (citizenData.getEntity().get().getCitizenDiseaseHandler().isSick()) {
                     continue;
                 }
 
-                if (citizenData.getEntity().isPresent())
-                {
-                    if (citizenData.getEntity().get().getCitizenDiseaseHandler().isSick())
-                    {
-                        continue;
-                    }
-
-                    citizenData.getEntity().get().remove(Entity.RemovalReason.DISCARDED);
-                }
-
-                colony.getCitizenManager().spawnOrCreateCivilian(citizenData, world, player.blockPosition(), true);
-                citizenData.setNextRespawnPosition(buildingPos);
-
-                building.getSetting(AbstractBuildingGuards.GUARD_TASK).set(GuardTaskSetting.FOLLOW);
-                ((AbstractBuildingGuards) building).setPlayerToFollow(player);
-                final GuardFollowModeSetting grouping = building.getSetting(AbstractBuildingGuards.FOLLOW_MODE);
-                if (grouping.getValue().equals(GuardFollowModeSetting.LOOSE))
-                {
-                    grouping.trigger();
-                }
-
-                citizenData.setSaturation(100);
-
-                colony.getPackageManager().addCloseSubscriber(player);
-
-                if (job != null && job.getWorkerAI() != null)
-                {
-                    final long spawnTime = world.getGameTime() + TICKS_SECOND * 900;
-
-                    // Timed despawn
-                    job.getWorkerAI().registerTarget(new AIOneTimeEventTarget(() ->
-                    {
-                        if (world.getGameTime() - spawnTime > 0)
-                        {
-                            ((AbstractBuildingGuards) building).getSetting(AbstractBuildingGuards.GUARD_TASK).set(GuardTaskSetting.PATROL);
-                            citizenData.getEntity().ifPresent(e -> e.remove(Entity.RemovalReason.DISCARDED));
-                            colony.getPackageManager().removeCloseSubscriber(player);
-                            return true;
-                        }
-                        return false;
-                    }
-                      , AIWorkerState.DECIDE));
-                }
+                citizenData.getEntity().get().remove(Entity.RemovalReason.DISCARDED);
             }
 
-            SoundUtils.playSoundForPlayer(player, SoundEvents.UI_TOAST_CHALLENGE_COMPLETE, 0.3f, 1.0f);
+            colony.getCitizenManager().spawnOrCreateCivilian(citizenData, world, player.blockPosition(), true);
+            citizenData.setNextRespawnPosition(building.getPosition());
+
+            building.getSetting(AbstractBuildingGuards.GUARD_TASK).set(GuardTaskSetting.FOLLOW);
+            ((AbstractBuildingGuards) building).setPlayerToFollow(player);
+            final GuardFollowModeSetting grouping = building.getSetting(AbstractBuildingGuards.FOLLOW_MODE);
+            if (grouping.getValue().equals(GuardFollowModeSetting.LOOSE)) {
+                grouping.trigger();
+            }
+
+            citizenData.setSaturation(100);
+
+            colony.getPackageManager().addCloseSubscriber(player);
+
+            if (job != null && job.getWorkerAI() != null) {
+                final long spawnTime = world.getGameTime() + lastTime;
+
+                // Timed despawn
+                job.getWorkerAI().registerTarget(new AIOneTimeEventTarget(() ->
+                {
+                    if (world.getGameTime() - spawnTime > 0) {
+                        building.getSetting(AbstractBuildingGuards.GUARD_TASK).set(GuardTaskSetting.PATROL);
+                        citizenData.getEntity().ifPresent(e -> e.remove(Entity.RemovalReason.DISCARDED));
+                        colony.getPackageManager().removeCloseSubscriber(player);
+                        return true;
+                    }
+                    return false;
+                }
+                        , AIWorkerState.DECIDE));
+            }
         }
-
-
-        return itemStack;
     }
 
     @Override
